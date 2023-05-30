bcea366467f709253511b35d4233c362600b347c
Tanuj Kalonia <tanujkalonia2002@gmail.com>
2023-05-30 22:52:05 +0530

    Added 'Booking' model by using the following command : ``` npx sequelize model:generate --name Booking --attributes flightId:integer,userId:integer,status:enum,totalCost:integer ```

---
+++ src/migrations/20230530142909-create-booking.js
@@ -0,0 +1,45 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Bookings', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      flightId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      userId: {
+        type: Sequelize.INTEGE,
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.ENUM,
+        allowNull: false,
+      },
+      noOfSeats: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      totalCost: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Bookings');
+  }
+};
