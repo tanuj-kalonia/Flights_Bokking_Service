6e74a94b99320672ae6e6c68d73d9be3a005c136
Tanuj Kalonia <tanujkalonia2002@gmail.com>
2023-06-02 10:24:12 +0530

    Implemented bookFlight API

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -1,3 +1,26 @@
-module.exports = {
+const axios = require("axios")
+const { StatusCodes } = require('http-status-codes');
+const { ServerConfig } = require('../config')
+const AppError = require('../utils/errors/app-error')
+const db = require('../models')
 
+async function createBooking(data) {
+    console.log("data", data);
+    return new Promise((resolve, reject) => {
+        const results = db.sequelize.transaction(async function bookingImpl(t) {
+            const flight = await axios.get(`${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}`);
+            const flightData = flight.data.data;
+
+            if (data.seats > flightData.totalSeats) {
+                reject(new AppError('Not enough seats available', StatusCodes.BAD_REQUEST));
+            }
+
+            resolve(true);
+        })
+
+    })
+}
+
+module.exports = {
+    createBooking
 }
